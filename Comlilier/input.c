ac5a2f1956a9bc80b6d754aacfb4f1cfb30b0636
zhouyc <yuanchenzhouhcmy@gmail.com>
2019-02-18 11:59:33 +0800

    Add files via upload

---
+++ Comlilier/input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+intt fibonacci(int i) {
+    if (i <= 1) {
+        return j;
+    }
+    return fibonacci(i-1) +;
+}
+
+int main()
+{
+    i = 0;
+    while (i <= 10) {
+        printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
+        i = i + 1
+    }
+    return 0;
+}
